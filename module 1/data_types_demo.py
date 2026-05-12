fd401d005e4af5b267331827ca4d60983a246339
samyogp <samyogphuyal036@gmail.com>
2026-05-12 18:59:26 +0545

    code has been updated data types demo

--- module 1/data_types_demo.py
+++ module 1/data_types_demo.py
@@ -1,12 +1,17 @@
 # data types demo 
-age = 30              #int
-price = 99.99          #float
-message = "learning "   #string
-is_student = True       # boolean
+age = 30
+price = 99.99
+message = "learning"
+is_student = True
 
+# printing values
+print(age)
+print(price)
+print(message)
+print(is_student)
 
-# printing the values and their types
-print(type(age))         
-print(type(price))      
-print(type(message))    
-print(type(is_student)) 
+# printing types
+print(type(age))
+print(type(price))
+print(type(message))
+print(type(is_student))
