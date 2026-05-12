a8bebff2bbf1ff1cf0e83a47e9a6889a162c6fbb
samyogp <samyogphuyal036@gmail.com>
2026-05-12 18:58:44 +0545

    data types demo

---
+++ module 1/data_types_demo.py
@@ -0,0 +1,12 @@
+# data types demo 
+age = 30              #int
+price = 99.99          #float
+message = "learning "   #string
+is_student = True       # boolean
+
+
+# printing the values and their types
+print(type(age))         
+print(type(price))      
+print(type(message))    
+print(type(is_student)) 
