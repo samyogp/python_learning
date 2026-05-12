48c1180309b63ab13ac6f5f9743a097117b732f4
samyogp <samyogphuyal036@gmail.com>
2026-05-12 17:28:05 +0545

    intial commit

---
+++ samyog.py
@@ -0,0 +1,3 @@
+print (" Hello Samyog Bhai ! Welcome to the world of Python Programming. ")
+print (" I am your Python Assistant. I will help you to learn Python Programming. ")
+print (" Let's start with the basics of Python Programming. ")
