f40bda304092c7179b9d7cbd5fdd5947b0e399e7
samyogp <samyogphuyal036@gmail.com>
2026-05-16 15:52:33 +0545

    this is the multiplication example

--- module 2/multiplication.py
+++ module 2/multiplication.py
@@ -4,3 +4,18 @@ price = 100
 quantity = 5
 total = price * quantity
 print(f"Total price of the {price} and {quantity} is: {total}")
+
+
+# another example of the multiplication where user can input the numbers 
+
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+
+# converting to the input to the float for the calculation 
+num1 = float(num1)
+num2 = float(num2)
+
+
+product = num1 * num2
+print(f" The product of {num1} and {num2} is: {product}")
