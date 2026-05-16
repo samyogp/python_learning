4baaee58039e898881d93e2ad7e1d2f889436bc0
samyogp <samyogphuyal036@gmail.com>
2026-05-16 15:41:58 +0545

    addition exaples

--- module 2/Addition.py
+++ module 2/Addition.py
@@ -4,3 +4,14 @@ b = 5
 
 result = a + b
 print(result) # 15
+# another example of addition where user can input the numbers
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+#converting the input to float for the calculation
+num1 = float(num1)
+num2 = float(num2)
+
+
+sum = num1 + num2
+print(f"The sum of {num1} and {num2} is: {sum}")
