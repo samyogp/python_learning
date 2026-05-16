e978e4dab656ab08bea9187f87e6439e3887389a
samyogp <samyogphuyal036@gmail.com>
2026-05-16 15:46:30 +0545

    initial commit

--- module 2/Subtraction.py
+++ module 2/Subtraction.py
@@ -3,3 +3,16 @@ a = 10
 b = 20
 sum = a - b
 print(sum)  # -10
+
+
+# another example of subtraction where user can input the numbers
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+
+# converting to the input to the float for the calculation 
+num1 = float(num1)
+num2 = float(num2)
+
+difference = num1 - num2
+print(f" The difference between {num1} and {num2} is: {difference}")
