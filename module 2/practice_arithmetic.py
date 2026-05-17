7eda383ca951a9bbf43308b17a36f97e045e19cd
samyogp <samyogphuyal036@gmail.com>
2026-05-17 19:31:28 +0545

    addition example

---
+++ module 2/practice_arithmetic.py
@@ -0,0 +1,10 @@
+# practice arithmetic operations
+a = input("Enter the number: ")
+b = input("Enter the number: ")
+
+# converting the input to float for the calculation 
+a = float(a)
+b = float(b)
+
+# addition 
+print(f"{a} + {b} = {a + b}")
