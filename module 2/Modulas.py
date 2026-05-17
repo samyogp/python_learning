7722fb5a0191817ff2488ef8ca11837c1e6e894d
samyogp <samyogphuyal036@gmail.com>
2026-05-17 14:00:47 +0545

    modulus code has been updated

--- module 2/Modulas.py
+++ module 2/Modulas.py
@@ -1,5 +1,7 @@
 # modulus % (remainder after division)
 
-print(10 % 3) 
-print(17 % 5)
-print(20 % 4)
+print(10 % 3)  # 1 (10 divided by 3 is 3 with a remainder of 1)
+
+print(17 % 5) # 2 (17 divided by 5 is 3 with a remainder of 2)
+print(20 % 4)  # 0 (20 divided by 4 is 5 with no remainder)
+print(-10 % 3) # 2 ( -10 divided by 3 is -4 with a remainder of 2)
