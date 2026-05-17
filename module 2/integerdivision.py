9cc563a5e003a2e32c2a4380d1244e86c2409661
samyogp <samyogphuyal036@gmail.com>
2026-05-17 13:56:15 +0545

    initial commit

---
+++ module 2/integerdivision.py
@@ -0,0 +1,12 @@
+# // integer division example 
+
+print(10 // 2) # 5 (integer)
+print(20 // 6) # 3 (integer, not 3.3333)
+print(-10 // 3) # -4 (integer, not -3.3333)
+
+
+# another example of integer division where user can input the numbers 
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+# converting the input to float for the calculation 
