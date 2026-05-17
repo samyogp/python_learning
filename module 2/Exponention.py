8077060e0bcc6daac5ff04ab7809d7b2bebce8cc
samyogp <samyogphuyal036@gmail.com>
2026-05-17 19:28:31 +0545

    exponent example where user can input the number

--- module 2/Exponention.py
+++ module 2/Exponention.py
@@ -3,3 +3,18 @@ print(2 ** 3) # 8
 print(5 ** 2) # 25
 print(10 ** 4) # 10000
 print(9 ** 0.5) # 3.0 (square root of 9) 
+
+
+# another example of exponent where user can input the base and the exponent 
+base = input("Enter the base number: ")
+exponent = input("Enter the exponent: ")
+
+# converting the input to float for the calculation 
+base = float(base)
+exponent = float(exponent)
+
+# performing exponentiation
+
+calculation = base ** exponent
+print(f"The result of {base} raised to the power of {exponent} is: {calculation}")
+
