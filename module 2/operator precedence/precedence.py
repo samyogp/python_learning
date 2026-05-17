05c777e4173206cc660d65a2a4354329baf634c8
samyogp <samyogphuyal036@gmail.com>
2026-05-17 19:46:56 +0545

    intial commit

--- module 2/operator precedence/precedence.py
+++ module 2/operator precedence/precedence.py
@@ -4,4 +4,15 @@ print(2 + 3 * 3) # 11 (multiplication is performed before addition)
 print((2 + 3) * 4) #20 (parenthesis first: 5*4)
 print(10 - 2 * 3) # 4 (2*3=6, then 10-6
 print(2 ** 3 ** 2) # 512 (right to left: 3**2=9, then 2**9 )
- 
+
+
+
+
+a = input("Enter a number: ")
+b = input("Enter another number: ")
+
+a = float(a)
+b = float(b)
+
+result = a + b * 2 # multiplication first, then addition
+print(f"The result of {a} + {b} * 2 is: {result}")
