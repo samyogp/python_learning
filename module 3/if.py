bc7c8259334a28d472752ee805d360a8d9a18468
samyogp <samyogphuyal036@gmail.com>
2026-05-18 16:42:13 +0545

    if condition examples were added

---
+++ module 3/if.py
@@ -0,0 +1,15 @@
+# if example 
+age = 18
+if age >= 18:
+    print("You can vote!")
+
+
+    # another example with the user input 
+    age = input("Enter your age: ")
+    age = int(age)
+
+    if age >= 18:
+        print("you can vote!")
+    else: 
+        print("you cannot vote yet.")
+        
