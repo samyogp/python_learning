4e5b76b0b6b5aa115af710b9806bf1e5ea6211a0
samyogp <samyogphuyal036@gmail.com>
2026-05-19 17:40:12 +0545

    while loop example

---
+++ module 4/while_loop.py
@@ -0,0 +1,9 @@
+# syntax 
+# while condition:
+    # code to repeat while condition is true
+    # make sure condition becomes false eventually to avoid infinite loop
+# example of while loop in python
+count = 0
+while count <= 13:
+    print(count)
+    count = count + 1
