a7e48fc94d24e7982976640fe5d5b89fd26f9183
samyogp <samyogphuyal036@gmail.com>
2026-05-19 21:55:31 +0545

    for letter in the word demo

--- for_string.py
+++ for_string.py
@@ -2,4 +2,10 @@
 word = "Nepal"
 for letter in word:
     print(letter)
-    
+
+
+    # another example 
+    word = input("Enter a word: ")
+    for letter in word:
+        print(letter)
+        
