d244e3bfc1590d344aeac1f43a2cbe3e67f0c75b
samyogp <samyogphuyal036@gmail.com>
2026-05-23 16:14:33 +0545

    indexing example

--- module 5/indexing1.py
+++ module 5/indexing1.py
@@ -5,9 +5,12 @@ user_list = []
 for _ in range(num_items):
     item = input("Enter an item: ")
     user_list.append(item)
-# accessing elements using positive indexing
-print(fruits[0])  # output apple
-print(fruits[2])  # output will be mango
-# accessing elements using negative indexing
-print(user_list[-1])  # output will be the last item added
-print(user_list[-3]) # output will be the third last item added
+print("Your list:", user_list)
+# accessing elements in the list using indexing
+index = input("Enter the index of the item you want to access (starting from 0): ")
+index = int(index)
+if 0 <= index < len(user_list):
+    print(f"The item at index {index} is: {user_list[index]}")
+else:
+    print("Invalid index. Please enter a number between 0 and", len(user_list)-1)
+    
