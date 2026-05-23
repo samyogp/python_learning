43a3d10f378f44ee0758f18c465ac61499bed2db
samyogp <samyogphuyal036@gmail.com>
2026-05-23 19:29:11 +0545

    removing banana from the lis tusing remove

---
+++ module 5/modifying lists/remove_items.py
@@ -0,0 +1,5 @@
+# removed items in modifying lists using remove and pop and del
+# using remove to remove an list item by value.
+fruits = ["apple", "banana", "cherry", "banana"]
+fruits.remove("banana")
+print(fruits)
