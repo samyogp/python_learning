be443c851b8a2a5c2cd9e6fe45e34e71c316fe4b
samyogp <samyogphuyal036@gmail.com>
2026-05-23 19:23:16 +0545

    adding items example

--- module 5/modifying lists/add_items.py
+++ module 5/modifying lists/add_items.py
@@ -1,1 +1,7 @@
 # modifying lists and add items using append and insert
+colors = ["red", "green", "blue"]
+colors.append("green")
+print(colors)
+# insert using
+colors.insert(1, "yellow")
+print(colors)
