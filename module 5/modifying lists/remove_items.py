4aab6151478b60a0a134488298c134bbb3bb9596
samyogp <samyogphuyal036@gmail.com>
2026-05-23 19:34:52 +0545

    remove list example

--- module 5/modifying lists/remove_items.py
+++ module 5/modifying lists/remove_items.py
@@ -1,5 +1,15 @@
 # removed items in modifying lists using remove and pop and del
 # using remove to remove an list item by value.
-fruits = ["apple", "banana", "cherry", "banana"]
-fruits.remove("banana")
+fruits = ["apple", "banana", "mango", "banana"] 
+fruits.remove("banana") # removes first banana only
+print(fruits)  # ['apple' , 'mango' , 'banana']
+
+# pop(index) removes and returns item at index (default last )
+last = fruits.pop()    # removes and returns 'banana'
+print(last) # banana
+print(fruits)  # ['apple, mango]
+
+# using del
+del fruits[0]
 print(fruits)
+ 
