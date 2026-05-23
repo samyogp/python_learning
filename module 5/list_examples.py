9bc023050f9336bb194aa42d3cacff8f6732b593
samyogp <samyogphuyal036@gmail.com>
2026-05-23 15:24:16 +0545

    lists example via user input

--- module 5/list_examples.py
+++ module 5/list_examples.py
@@ -4,3 +4,13 @@ numbers = [10, 20, 30, 40]
 mixed = [1, "hello", 3.14, True]
 # An empty list
 empty = []
+
+# another example of list with user input
+num_items = input("How many items do you want to add to the list?")
+num_items = int(num_items)
+user_list = []
+for _ in range(num_items):
+    item = input("Enter an item: ")
+    user_list.append(item)
+    print(f"Current list: {user_list}")
+print(f"Final list: {user_list}")
