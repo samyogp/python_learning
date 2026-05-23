d81d80d32a82e9813f7578ea02a93eacd06fc936
samyogp <samyogphuyal036@gmail.com>
2026-05-23 15:28:45 +0545

    indexing examples

---
+++ module 5/indexing.py
@@ -0,0 +1,8 @@
+# indexing example in python
+fruits = ["apple", "banana", "mango", "orange", "grapes"]
+# accessing elements using positive indexing
+print(fruits[0])  # output apple
+print(fruits[2])  # output will be mango
+# accessing elements using negative indexing
+print(fruits[-1])  # output will be grapes
+print(fruits[-3]) # output will be mango
