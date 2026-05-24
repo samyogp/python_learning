36cd67a32dcb0b48cb619c0d93b1fc025709c8c3
samyogp <samyogphuyal036@gmail.com>
2026-05-24 21:37:05 +0545

    todo list

--- module 5/practice exercises/todo.py
+++ module 5/practice exercises/todo.py
@@ -1,13 +1,28 @@
-# manage todo list 
+# todo.py
 todo_list = []
 while True:
     print("\n1. Add task")
-    print("2. VIew tasks")
+    print("2. View tasks")
     print("3. Remove task")
-    print("4. exit")
-    choice = input("choose: ")
+    print("4. Exit")
+    choice = input("Choose: ")
+    
     if choice == "1":
         task = input("Enter task: ")
         todo_list.append(task)
         print("Task added!")
-        
+    elif choice == "2":
+        print("\nYour tasks:")
+        for i, task in enumerate(todo_list, 1):
+            print(f"{i}. {task}")
+    elif choice == "3":
+        task_num = int(input("Task number to remove: "))
+        if 1 <= task_num <= len(todo_list):
+            removed = todo_list.pop(task_num - 1)
+            print(f"Removed: {removed}")
+        else:
+            print("Invalid number")
+    elif choice == "4":
+        break
+    else:
+        print("Invalid choice")
