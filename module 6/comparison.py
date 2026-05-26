ac347957f69f6912542eb50b42f7f379665f0ed4
samyogp <samyogphuyal036@gmail.com>
2026-05-26 22:27:10 +0545

    tuple vs list

---
+++ module 6/comparison.py
@@ -0,0 +1,11 @@
+# tuple vs list - which one 
+# use list for: changing data 
+
+shopping_list = ["milk", "eggs", "bread"]
+shopping_list.append("butter")
+
+# use tuple for : fixed data 
+days_of_week = ("mon", "tue", "wed", "thu", "fri", "sat", "sun")
+
+# coordinates, RGB colors, constants
+rgb = (225, 128, 0)
