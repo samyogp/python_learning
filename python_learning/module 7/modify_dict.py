7ceb3ffc97403dd5c9d876833a1e5b65026e696f
samyogp <samyogphuyal036@gmail.com>
2026-06-13 17:07:23 +0545

    modify example added

---
+++ python_learning/module 7/modify_dict.py
@@ -0,0 +1,6 @@
+# modify example in dictionaries
+student = {"name": "samyog" , "age": 19}
+student["age"] = 21     # this is the modify existing key
+student["city"] = "kathmandu"   # add new key value
+student["city"] = "pokhara"  
+print(student)
