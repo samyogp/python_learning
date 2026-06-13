268d83b160f21970628ca6d7c1adebfadc075f39
samyogp <samyogphuyal036@gmail.com>
2026-06-13 17:56:34 +0545

    code has been updated

--- python_learning/module 7/nested_dict.py
+++ python_learning/module 7/nested_dict.py
@@ -1,4 +1,7 @@
 # nested dictionaries example
+import math
+
+
 students = {
     "ram": {"math": 85, "science": 90},
     "sita": {"math": 78, "science": 88},
@@ -9,5 +12,10 @@ students = {
 print(students["ram"]["math"])
 print(students["sita"]["science"])
 print(students["gita"]["math"])
+
+
+print(f"Ram math: {students['ram']['math']} and science: {students['ram']['science']}")
+
+
 
 
