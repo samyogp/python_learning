3656774bce95c4c4f89b6f2829898140222f41fe
samyogp <samyogphuyal036@gmail.com>
2026-06-13 17:51:35 +0545

    nested example added

---
+++ python_learning/module 7/nested_dict.py
@@ -0,0 +1,13 @@
+# nested dictionaries example
+students = {
+    "ram": {"math": 85, "science": 90},
+    "sita": {"math": 78, "science": 88},
+    "gita": {"math": 92, "science": 85}
+
+}
+
+print(students["ram"]["math"])
+print(students["sita"]["science"])
+print(students["gita"]["math"])
+
+
