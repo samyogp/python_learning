be41cb63bd1334f715ab5578dfefbb51cd89c712
samyogp <samyogphuyal036@gmail.com>
2026-06-13 17:45:06 +0545

    in operator example in python

---
+++ python_learning/module 7/in_dict.py
@@ -0,0 +1,6 @@
+# this is the checking if key exist in operator
+
+# in_dict.py
+inventory = {"apples": 10, "bananas": 5}
+if "apples" in inventory:
+    print(f"We have {inventory['apples']} apples")
