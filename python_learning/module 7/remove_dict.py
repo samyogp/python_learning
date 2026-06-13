94e1c9f87a26d5b3317425f0f8aa9a3861257a05
samyogp <samyogphuyal036@gmail.com>
2026-06-13 17:10:53 +0545

    code added in removed dictionary file

--- python_learning/module 7/remove_dict.py
+++ python_learning/module 7/remove_dict.py
@@ -2,3 +2,12 @@ scores = {"math": 56, "science": 78, "english": 90}
 removed = scores.pop("science")
 print(scores)
 print(removed)
+
+
+last = scores.popitem()
+print(last)
+print(scores)
+
+
+scores.clear()
+print(scores)
