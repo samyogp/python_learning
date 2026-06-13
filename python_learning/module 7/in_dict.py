288c8c9e4679d9b39455e0bece3c82768427eb9f
samyogp <samyogphuyal036@gmail.com>
2026-06-13 17:48:26 +0545

    fruits example in in operator with via user input

--- python_learning/module 7/in_dict.py
+++ python_learning/module 7/in_dict.py
@@ -4,3 +4,12 @@
 inventory = {"apples": 10, "bananas": 5}
 if "apples" in inventory:
     print(f"We have {inventory['apples']} apples")
+
+    # user input example
+    stock = {"apples": 10, "bananas": 5, "oranges": 8}
+    fruit = input("Enter a fruit to check: ")
+    if fruit in stock:
+        print(f"We have {stock[fruit]} {fruit}")
+    else: 
+        print(f"Sorry, we don't have {fruit}")
+        
