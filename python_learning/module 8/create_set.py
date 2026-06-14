2b62653c726695828fb7bb8db8968c8b67b8ac81
samyogp <samyogphuyal036@gmail.com>
2026-06-14 08:45:23 +0545

    creating sets added

---
+++ python_learning/module 8/create_set.py
@@ -0,0 +1,9 @@
+# creating sets 
+
+#from a list (removes duplicates)
+list_with_dupes = [1, 2, 2, 3, 3, 3]
+unique_set = set(list_with_dupes)
+print(unique_set)
+
+#directly
+colors = {"red", "green", "blue"}
