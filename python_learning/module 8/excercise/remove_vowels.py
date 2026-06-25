b12848c8cf69fbbe5f06c74ead906d32d0d63511
samyogp <samyogphuyal036@gmail.com>
2026-06-25 16:17:51 +0545

    initial commit

--- python_learning/module 8/excercise/remove_vowels.py
+++ python_learning/module 8/excercise/remove_vowels.py
@@ -7,3 +7,10 @@ print(f"Without vowels: {result}")
 # printing hello from user
 user_input = input("please enter a any word: ")
 print(f"Hello, {user_input}!")
+# saying hungry to the waiter in hotel using python code
+hungry_input = input("Are you hungry? (yes/no): ")
+if hungry_input.lower() == "yes":
+    print("The waiter approaches and asks how they can help.")
+else:
+    print("The waiter nods and moves on.")
+    
