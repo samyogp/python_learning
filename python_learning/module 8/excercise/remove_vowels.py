1892de42904ed5368c679dfc701123d637608e16
samyogp <samyogphuyal036@gmail.com>
2026-06-25 16:15:41 +0545

    initial commit

--- python_learning/module 8/excercise/remove_vowels.py
+++ python_learning/module 8/excercise/remove_vowels.py
@@ -3,3 +3,7 @@ text = input("Enter a string: ")
 vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
 result = ''.join([ch for ch in text if ch not in vowels])
 print(f"Without vowels: {result}")
+
+# printing hello from user
+user_input = input("please enter a any word: ")
+print(f"Hello, {user_input}!")
