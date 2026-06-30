7fac2f3cd66efe58d67e13ff5ce94a2653cb3c21
samyogp <samyogphuyal036@gmail.com>
2026-06-30 15:40:21 +0545

    this is the for range example in the python of the module 4

---
+++ python_learning/Revisions/module4 revision/range_example.py
@@ -0,0 +1,9 @@
+# this is the range example in the python that are in the module 4
+# range_examples.py
+print("Range(5):")
+for i in range(5):
+    print(i)
+
+print("\nRange(2, 10, 2):")
+for i in range(2, 10, 2):
+    print(i)
